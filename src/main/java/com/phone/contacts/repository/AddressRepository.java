5ea8f385a9db7ca918a4cef8fe47cf3a6338b14b
Tatiana Domingue <tatiana.domingue@protonmail.com>
2022-01-31 20:07:05 -0500

    AddressRepository created

---
+++ src/main/java/com/phone/contacts/repository/AddressRepository.java
@@ -0,0 +1,8 @@
+package com.phone.contacts.repository;
+
+import com.phone.contacts.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
+
+}
