2aa3a7ded3bad52432507e796612e43591eccdbc
Tatiana Domingue <tatiana.domingue@protonmail.com>
2022-02-01 10:39:17 -0500

    getAllAddresses added

--- src/main/java/com/phone/contacts/controller/AddressController.java
+++ src/main/java/com/phone/contacts/controller/AddressController.java
@@ -1,9 +1,14 @@
 package com.phone.contacts.controller;
 
+import com.phone.contacts.model.Contact;
 import com.phone.contacts.service.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api")    // url -> http://localhost:9092/api
@@ -14,6 +19,12 @@ public class AddressController {
     @Autowired
     public void setAddressService(AddressService addressService) {
         this.addressService = addressService;
+    }
+
+    @GetMapping("/user/{userId}/contact/{contactId}/address")   // url -> http://localhost:9092/api/user/userId/contact/contactId/address
+    public List<Contact> getAllAddresses(@PathVariable Long contactId) {
+        System.out.println("calling getAllAddresses");
+        return addressService.getAllAddresses(contactId);
     }
 
 }
