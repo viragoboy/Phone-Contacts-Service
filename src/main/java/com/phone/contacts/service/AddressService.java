838c161bf7e43dc7b9978e16e76cafcb8f3bd9bd
Tatiana Domingue <tatiana.domingue@protonmail.com>
2022-02-01 10:02:37 -0500

    reference added

--- src/main/java/com/phone/contacts/service/AddressService.java
+++ src/main/java/com/phone/contacts/service/AddressService.java
@@ -1,8 +1,23 @@
 package com.phone.contacts.service;
 
+import com.phone.contacts.repository.AddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AddressService {
 
+    private AddressRepository addressRepository;
+
+    @Autowired
+    public void setAddressRepository(AddressRepository addressRepository) {
+        this.addressRepository = addressRepository;
+    }
+
+    private ContactService contactService;
+
+    @Autowired
+    public void setContactService(ContactService contactService) {
+        this.contactService = contactService;
+    }
 }
