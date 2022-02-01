3b1d18a0b98770675b6e8a8c9e1efe707cee2c93
Tatiana Domingue <tatiana.domingue@protonmail.com>
2022-02-01 17:47:15 -0500

    deleteAddress created

--- src/main/java/com/phone/contacts/controller/AddressController.java
+++ src/main/java/com/phone/contacts/controller/AddressController.java
@@ -41,4 +41,10 @@ public class AddressController {
         return addressService.updateAddress(userId, contactId, addressId, addressObject);
     }
 
+    @DeleteMapping("/user/{userId}/contact/{contactId}/address/{addressId}")   // url -> http://localhost:9092/api/user/userId/contact/contactId/address/addressId
+    public Address deleteAddress(@PathVariable Long userId, @PathVariable Long contactId, @PathVariable Long addressId) {
+        System.out.println("calling deleteAddress");
+        return addressService.deleteAddress(userId, contactId, addressId);
+    }
+
 }
