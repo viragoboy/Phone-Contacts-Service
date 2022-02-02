0f4ec8d8a7e586d19ef487fcdb2c197af012fde4
Tatiana Domingue <tatiana.domingue@protonmail.com>
2022-02-02 10:44:22 -0500

    PhoneController, PhoneService and PhoneRepository linked

--- src/main/java/com/phone/contacts/controller/PhoneController.java
+++ src/main/java/com/phone/contacts/controller/PhoneController.java
@@ -1,10 +1,19 @@
 package com.phone.contacts.controller;
 
+import com.phone.contacts.service.PhoneService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/api")    // url -> http://localhost:9092/api
 public class PhoneController {
+
+    private PhoneService phoneService;
+
+    @Autowired
+    public void setPhoneService(PhoneService phoneService) {
+        this.phoneService = phoneService;
+    }
 
 }
--- src/main/java/com/phone/contacts/service/PhoneService.java
+++ src/main/java/com/phone/contacts/service/PhoneService.java
@@ -1,8 +1,17 @@
 package com.phone.contacts.service;
 
+import com.phone.contacts.repository.PhoneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PhoneService {
+
+    private PhoneRepository phoneRepository;
+
+    @Autowired
+    public void setPhoneRepository(PhoneRepository phoneRepository) {
+        this.phoneRepository = phoneRepository;
+    }
 
 }
