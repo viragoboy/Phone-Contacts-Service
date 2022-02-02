c2436ec98e3a3cb017ff8ca67e58282a2061cf6d
Tatiana Domingue <tatiana.domingue@protonmail.com>
2022-02-02 10:48:02 -0500

    PhoneService linked to contactService and contactRepository

--- src/main/java/com/phone/contacts/service/PhoneService.java
+++ src/main/java/com/phone/contacts/service/PhoneService.java
@@ -1,5 +1,6 @@
 package com.phone.contacts.service;
 
+import com.phone.contacts.repository.ContactRepository;
 import com.phone.contacts.repository.PhoneRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,20 @@ public class PhoneService {
     @Autowired
     public void setPhoneRepository(PhoneRepository phoneRepository) {
         this.phoneRepository = phoneRepository;
+    }
+
+    private ContactService contactService;
+
+    @Autowired
+    public void setContactService(ContactService contactService) {
+        this.contactService = contactService;
+    }
+
+    private ContactRepository contactRepository;
+
+    @Autowired
+    public void setContactRepository(ContactRepository contactRepository) {
+        this.contactRepository = contactRepository;
     }
 
 }
