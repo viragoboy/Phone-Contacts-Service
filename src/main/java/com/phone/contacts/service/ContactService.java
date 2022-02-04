fd41ef4d920981b5e748ad36fde0d60a907cf70e
Tatiana Domingue <tatiana.domingue@protonmail.com>
2022-02-03 19:39:38 -0500

    deleteContact fixed

--- src/main/java/com/phone/contacts/service/ContactService.java
+++ src/main/java/com/phone/contacts/service/ContactService.java
@@ -3,6 +3,7 @@ package com.phone.contacts.service;
 import com.phone.contacts.exceptions.InformationExistException;
 import com.phone.contacts.exceptions.InformationNotFoundException;
 import com.phone.contacts.model.Contact;
+import com.phone.contacts.model.Phone;
 import com.phone.contacts.model.User;
 import com.phone.contacts.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,13 @@ public class ContactService {
     @Autowired
     public void setUserService(UserService userService) {
         this.userService = userService;
+    }
+
+    private PhoneService phoneService;
+
+    @Autowired
+    public void setPhoneService(PhoneService phoneService) {
+        this.phoneService = phoneService;
     }
 
     public List<Contact> getAllContacts(Long userId) {
@@ -68,8 +76,15 @@ public class ContactService {
 
     public Contact deleteContact(Long userId, Long contactId) {
         User user = userService.getUser(userId).get();
-        Contact contact  = contactRepository.getContactByUserAndContactId(user, contactId);
+        Contact contact = contactRepository.getContactByUserAndContactId(user, contactId);
         if (contact != null) {
+    //  The database is not allowing me to delete a contact that still has at least one phone number in it, so I'm iterating inside the phone list and deleting the phone(s) it contains:
+            if (!contact.getPhones().isEmpty()) {
+               List<Phone> phones = contact.getPhones();
+                for (Phone phone : phones) {
+                    phoneService.deletePhone(userId, contactId, phone.getPhoneId());
+                }
+            }
             contactRepository.delete(contact);
             return contact;
         } else {
